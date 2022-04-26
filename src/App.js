e145e28ce81a3d8cc20b65c488db59f0bdcf822c
marcsi19 <mariann19@gmail.com>
2022-04-25 19:12:51 -0700

    useState events mapping

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('Mariann')
+
   const [events, setEvents] = useState([
     {title: "Mariann's birthday bash", id: 1},
     {title: "bowser's live stream", id: 2},
     {title: "race on moo moo farm", id: 3}
   ])
-
-  const handleClick = () => {
-    setName('Maria')
-    console.log(name)
+  
+  const handleClick = (id) => {
+    setEvents(prevEvents =>{
+      return prevEvents.filter(event => id !== event.id)
+  })
   }
+  
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
-      {events.map((event) => (
+      {events.map((event, index) => (
         <div key={event.id}>
-          <h2>{event.title}</h2>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
         </div>
       ))}
     </div>
